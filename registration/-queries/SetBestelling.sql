bc2d2365cb60624f96dd7124837d60eff2a85235
Rob Tierolff <teh@borreh.nl>
2015-02-23 14:15:24 +0100

    duplicate invoer vermijden

--- registration/-queries/SetBestelling.sql
+++ registration/-queries/SetBestelling.sql
@@ -1,2 +1,6 @@
+/* prevent duplicates */
+DELETE FROM Bestelling 
+WHERE deelnemer = @deelnemer AND product = @product;
+/* actual order */
 INSERT INTO Bestelling(deelnemer, product)
 VALUES(@deelnemer, @product);
